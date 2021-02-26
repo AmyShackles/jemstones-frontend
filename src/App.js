1a0004bfa88881c23ee263c7fdc5a54213351c4f
Amy Shackles <amyshackles@gmail.com>
2021-02-25 23:44:49 -0800

    Changing nav names

--- src/App.js
+++ src/App.js
@@ -6,36 +6,34 @@ import { NavLink } from './components/NavLink.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <nav>
-          <NavLink to="/">Stones</NavLink>
-          <NavLink to="/col">Cols</NavLink>
-          <NavLink to="/ger">Gers</NavLink>
-          <NavLink to="/har">Hars</NavLink>
-          <NavLink to="/jam">Jams</NavLink>
-          <NavLink to="/jan">Jan</NavLink>
-          <NavLink to="/jem">Jems</NavLink>
-          <NavLink to="/jom">Joms</NavLink>
-          <NavLink to="/jum">Jums</NavLink>
-        </nav>
-      </header>
-      <main>
-        <Router>
-          <Leaderboard type="all stones" exact path="/" />
-          <Leaderboard type="colestones" path="/col" />
-          <Leaderboard type="gerstones" path="/ger" />
-          <Leaderboard type="harrystones" path="/har" />
-          <Leaderboard type="jamstones" path="/jam" />
-          <Leaderboard type="janstones" path="/jan" />
-          <Leaderboard type="jemstones" path="/jem" />
-          <Leaderboard type="jomstones" path="/jom" />
-          <Leaderboard type="jumstones" path="/jum" />
-
-        </Router>
-
-      </main>
-    </div>
+      <div className="App">
+          <header className="App-header">
+              <nav>
+                  <NavLink to="/">Stones</NavLink>
+                  <NavLink to="/col">Colestones</NavLink>
+                  <NavLink to="/ger">Gerstones</NavLink>
+                  <NavLink to="/har">Harrystones</NavLink>
+                  <NavLink to="/jam">Jamstones</NavLink>
+                  <NavLink to="/jan">Janstones</NavLink>
+                  <NavLink to="/jem">Jemstones</NavLink>
+                  <NavLink to="/jom">Jomstones</NavLink>
+                  <NavLink to="/jum">Jumstones</NavLink>
+              </nav>
+          </header>
+          <main>
+              <Router>
+                  <Leaderboard type="all stones" exact path="/" />
+                  <Leaderboard type="colestones" path="/col" />
+                  <Leaderboard type="gerstones" path="/ger" />
+                  <Leaderboard type="harrystones" path="/har" />
+                  <Leaderboard type="jamstones" path="/jam" />
+                  <Leaderboard type="janstones" path="/jan" />
+                  <Leaderboard type="jemstones" path="/jem" />
+                  <Leaderboard type="jomstones" path="/jom" />
+                  <Leaderboard type="jumstones" path="/jum" />
+              </Router>
+          </main>
+      </div>
   );
 }
 
