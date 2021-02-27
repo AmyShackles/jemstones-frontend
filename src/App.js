b56d2cf6bb4c3cfa91d4ecf646ef3a9dd2db2f43
Amy Shackles <amyshackles@gmail.com>
2021-02-27 11:48:45 -0800

    Adds Amystones

--- src/App.js
+++ src/App.js
@@ -1,40 +1,42 @@
-import React from 'react';
-import './App.css';
-import { Router } from '@reach/router';
-import { Leaderboard } from './components/Leaderboard.js'
-import { NavLink } from './components/NavLink.js';
+import React from "react";
+import "./App.css";
+import { Router } from "@reach/router";
+import { Leaderboard } from "./components/Leaderboard.js";
+import { NavLink } from "./components/NavLink.js";
 
 function App() {
-  return (
-      <div className="App">
-          <header className="App-header">
-              <nav>
-                  <NavLink to="/">Total</NavLink>
-                  <NavLink to="/col">Cole</NavLink>
-                  <NavLink to="/ger">Ger</NavLink>
-                  <NavLink to="/har">Harry</NavLink>
-                  <NavLink to="/jam">Jam</NavLink>
-                  <NavLink to="/jan">Jan</NavLink>
-                  <NavLink to="/jem">Jem</NavLink>
-                  <NavLink to="/jom">Jom</NavLink>
-                  <NavLink to="/jum">Jum</NavLink>
-              </nav>
-          </header>
-          <main>
-              <Router>
-                  <Leaderboard type="all stones" exact path="/" />
-                  <Leaderboard type="colestones" path="/col" />
-                  <Leaderboard type="gerstones" path="/ger" />
-                  <Leaderboard type="harrystones" path="/har" />
-                  <Leaderboard type="jamstones" path="/jam" />
-                  <Leaderboard type="janstones" path="/jan" />
-                  <Leaderboard type="jemstones" path="/jem" />
-                  <Leaderboard type="jomstones" path="/jom" />
-                  <Leaderboard type="jumstones" path="/jum" />
-              </Router>
-          </main>
-      </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <nav>
+                    <NavLink to="/">Total</NavLink>
+                    <NavLink to="/amy">Amy</NavLink>
+                    <NavLink to="/col">Cole</NavLink>
+                    <NavLink to="/ger">Ger</NavLink>
+                    <NavLink to="/har">Harry</NavLink>
+                    <NavLink to="/jam">Jam</NavLink>
+                    <NavLink to="/jan">Jan</NavLink>
+                    <NavLink to="/jem">Jem</NavLink>
+                    <NavLink to="/jom">Jom</NavLink>
+                    <NavLink to="/jum">Jum</NavLink>
+                </nav>
+            </header>
+            <main>
+                <Router>
+                    <Leaderboard type="all stones" exact path="/" />
+                    <Leaderboard type="amystones" path="/amy" />
+                    <Leaderboard type="colestones" path="/col" />
+                    <Leaderboard type="gerstones" path="/ger" />
+                    <Leaderboard type="harrystones" path="/har" />
+                    <Leaderboard type="jamstones" path="/jam" />
+                    <Leaderboard type="janstones" path="/jan" />
+                    <Leaderboard type="jemstones" path="/jem" />
+                    <Leaderboard type="jomstones" path="/jom" />
+                    <Leaderboard type="jumstones" path="/jum" />
+                </Router>
+            </main>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import allstone from "../images/allStones.webp";
 import allstonefb from "../images/allStones.png";
+import amystone from "../images/amystone.webp";
+import amystonefb from "../images/amystone.png";
 import colestone from "../images/colestone.webp";
 import colestonefb from "../images/colestone.png";
 import gerstone from "../images/gerstone.webp";
@@ -43,131 +45,145 @@ const Leaderboard = ({ type }) => {
  
     return leaderboard.length > 0 ? (
         <div className="leaderboard">
-            
-        <table>
-            <caption>{type.slice(0, -1).toUpperCase()} LEADERBOARD</caption>
-                <thead><tr>
-                    <td></td>
-                    <th scope="col">Colestones</th>
-                    <th scope="col">Gerstones</th>
-                    <th scope="col">Harrystones</th>
-                    <th scope="col">Jamstones</th>
-                    <th scope="col">Janstones</th>
-                    <th scope="col">Jemstones</th>
-                    <th scope="col">Jomstones</th>
-                    <th scope="col">Jumstones</th>
-                    <th scope="col">Total Stones</th>
-                </tr>
+            <table>
+                <caption>{type.slice(0, -1).toUpperCase()} LEADERBOARD</caption>
+                <thead>
+                    <tr>
+                        <td></td>
+                        <th scope="col">Amystones</th>
+                        <th scope="col">Colestones</th>
+                        <th scope="col">Gerstones</th>
+                        <th scope="col">Harrystones</th>
+                        <th scope="col">Jamstones</th>
+                        <th scope="col">Janstones</th>
+                        <th scope="col">Jemstones</th>
+                        <th scope="col">Jomstones</th>
+                        <th scope="col">Jumstones</th>
+                        <th scope="col">Total Stones</th>
+                    </tr>
                 </thead>
                 <tbody>
-                {leaderboard.map((leader, index) => {
-                    return (
-                        <tr key={index}>
-                            <th scope="row" className="displayName">
-                                <img
-                                    src={leader.image}
-                                    alt={leader.display_name}
-                                    title={leader.display_name}
-                                />
-                                <br />
-                                {leader.display_name}
-                            </th>
-                            <td>
-                                <Image
-                                    src={colestone}
-                                    fallback={colestonefb}
-                                    alt="Colestone"
-                                    title="Colestones"
-                                />
-                                <br />
-                                {leader.colestones}
-                            </td>
-                            <td>
-                                <Image
-                                    src={gerstone}
-                                    fallback={gerstonefb}
-                                    alt="Gerstone"
-                                    title="Gerstones"
-                                />
-                                <br />
-                                {leader.gerstones}
-                            </td>
-                            <td>
-                                <Image
-                                    src={harrystone}
-                                    fallback={harrystonefb}
-                                    alt="Harrystone"
-                                    title="Harrystones"
-                                />
-                                <br />
-                                {leader.harrystones}
-                            </td>
-                            <td>
-                                <Image
-                                    src={jamstone}
-                                    fallback={jamstonefb}
-                                    alt="Jamstone"
-                                    title="Jamstones"
-                                />
-                                <br />
-                                {leader.jamstones}
-                            </td>
-                            <td>
-                                <Image
-                                    src={janstone}
-                                    fallback={janstonefb}
-                                    alt="Janstone"
-                                    title="Janstones"
-                                />
-                                <br />
-                                {leader.janstones}
-                            </td>
-                            <td>
-                                <Image
-                                    src={jemstone}
-                                    fallback={jemstonefb}
-                                    alt="Jemstone"
-                                    title="Jemstones"
-                                />
-                                <br />
-                                {leader.jemstones}
-                            </td>
-                            <td>
-                                <Image
-                                    src={jomstone}
-                                    fallback={jomstonefb}
-                                    alt="Jomstone"
-                                    title="Jomstones"
-                                />
-                                <br />
-                                {leader.jomstones}
-                            </td>
-                            <td>
-                                <Image
-                                    src={jumstone}
-                                    fallback={jumstonefb}
-                                    alt="Jumstone"
-                                    title="Jumstones"
-                                />
-                                <br />
-                                {leader.jumstones}
-                            </td>
-                            <td>
-                                <Image
-                                    src={allstone}
-                                    fallback={allstonefb}
-                                    alt="All Stones"
-                                    title="Total stones"
-                                />
-                                <br />
-                                {leader.stones}
-                            </td>
-                        </tr>
-                    );})}
+                    {leaderboard.map((leader, index) => {
+                        return (
+                            <tr key={index}>
+                                <th scope="row" className="displayName">
+                                    <img
+                                        src={leader.image}
+                                        alt={leader.display_name}
+                                        title={leader.display_name}
+                                    />
+                                    <br />
+                                    {leader.display_name}
+                                </th>
+                                <td>
+                                    <Image
+                                        src={amystone}
+                                        fallback={amystonefb}
+                                        alt="Amystone"
+                                        title="Amystones"
+                                    />
+                                    <br />
+                                    {leader.amystones}
+                                </td>
+                                <td>
+                                    <Image
+                                        src={colestone}
+                                        fallback={colestonefb}
+                                        alt="Colestone"
+                                        title="Colestones"
+                                    />
+                                    <br />
+                                    {leader.colestones}
+                                </td>
+                                <td>
+                                    <Image
+                                        src={gerstone}
+                                        fallback={gerstonefb}
+                                        alt="Gerstone"
+                                        title="Gerstones"
+                                    />
+                                    <br />
+                                    {leader.gerstones}
+                                </td>
+                                <td>
+                                    <Image
+                                        src={harrystone}
+                                        fallback={harrystonefb}
+                                        alt="Harrystone"
+                                        title="Harrystones"
+                                    />
+                                    <br />
+                                    {leader.harrystones}
+                                </td>
+                                <td>
+                                    <Image
+                                        src={jamstone}
+                                        fallback={jamstonefb}
+                                        alt="Jamstone"
+                                        title="Jamstones"
+                                    />
+                                    <br />
+                                    {leader.jamstones}
+                                </td>
+                                <td>
+                                    <Image
+                                        src={janstone}
+                                        fallback={janstonefb}
+                                        alt="Janstone"
+                                        title="Janstones"
+                                    />
+                                    <br />
+                                    {leader.janstones}
+                                </td>
+                                <td>
+                                    <Image
+                                        src={jemstone}
+                                        fallback={jemstonefb}
+                                        alt="Jemstone"
+                                        title="Jemstones"
+                                    />
+                                    <br />
+                                    {leader.jemstones}
+                                </td>
+                                <td>
+                                    <Image
+                                        src={jomstone}
+                                        fallback={jomstonefb}
+                                        alt="Jomstone"
+                                        title="Jomstones"
+                                    />
+                                    <br />
+                                    {leader.jomstones}
+                                </td>
+                                <td>
+                                    <Image
+                                        src={jumstone}
+                                        fallback={jumstonefb}
+                                        alt="Jumstone"
+                                        title="Jumstones"
+                                    />
+                                    <br />
+                                    {leader.jumstones}
+                                </td>
+                                <td>
+                                    <Image
+                                        src={allstone}
+                                        fallback={allstonefb}
+                                        alt="All Stones"
+                                        title="Total stones"
+                                    />
+                                    <br />
+                                    {leader.stones}
+                                </td>
+                            </tr>
+                        );
+                    })}
                 </tbody>
-        </table>
+            </table>
         </div>
-    ) : <></>
+    ) : (
+        <></>
+    );
 }
 
 export { Leaderboard }
